5139894fd95ef65571223042e9f401c3c0a35e30
gulomovff-lgtm <gulomovff@icloud.com>
2026-02-07 12:16:13 +0500

    Remove all files

--- src/components/ui/index.js
+++
@@ -1,5 +0,0 @@
-// UI Components barrel export
-export { default as Button } from './Button';
-export { default as Card } from './Card';
-export { default as Notification } from './Notification';
-export { default as NavItem } from './NavItem';
--- src/utils/index.js
+++
@@ -1,3 +0,0 @@
-// Utils barrel export
-export * from './helpers';
-export * from './telegram';
